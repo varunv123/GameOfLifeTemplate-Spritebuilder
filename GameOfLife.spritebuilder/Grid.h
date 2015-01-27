68e4061be2c26bf1029593a90ee1e56115a6bf20
varun <varunv123@gmail.com>
2015-01-27 17:31:17 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.h
+++ GameOfLife.spritebuilder/Grid.h
@@ -11,5 +11,9 @@
 @interface Grid : CCSprite
 @property (nonatomic, assign) int totalAlive;
 @property (nonatomic, assign) int generation;
+- (void) evolveStep;
+- (void) countNeighbors;
+- (void) updateCreatures;
+
 
 @end
