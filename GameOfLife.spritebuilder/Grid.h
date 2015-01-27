009959fccd8bff66b2822f6a3564afeca9b44d19
varun <varunv123@gmail.com>
2015-01-27 17:05:16 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Grid.h
@@ -0,0 +1,15 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Varun Vijayraghavan on 1/27/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+@property (nonatomic, assign) int totalAlive;
+@property (nonatomic, assign) int generation;
+
+@end
